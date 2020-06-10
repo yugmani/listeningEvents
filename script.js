a97f4dda4b04031daa6074f6193e8cbb8976fa18
Himalaya <yugmani@yahoo.com>
2020-06-09 23:59:37 -0700

    files created

---
+++ script.js
@@ -0,0 +1,43 @@
+var eventType = document.querySelector("#event-type"); 
+var mouseEventsEl = document.querySelector("#click-events");
+var keyEventsEl = document.querySelector("#key-events");
+
+
+
+function toggleDisplay(event) {
+  var value = event.target.value;
+  if(value === "keydown") {
+    mouseEventsEl.classList.add("hide");
+    keyEventsEl.classList.remove("hide");
+  }
+  else {
+    mouseEventsEl.classList.remove("hide");
+    keyEventsEl.classList.add("hide");
+  }
+}
+
+function keydown(event) {
+  var keyPress = event.key;
+  var keyCode = event.code;
+  document.querySelector("#key").textContent = keyPress;
+  document.querySelector("#code").textContent = keyCode;
+  document.querySelector("#status").textContent = "KEYDOWN Event";
+}
+
+function keyup(event){
+document.querySelector("#status").textContent = "KEYUP Event";
+}
+
+function click(event){
+  var target = event.target.textContent;
+  var x = event.clientX;
+  var y = event.clientY;
+  document.querySelector("#target").innerHTML = `<h3>${target}</h3>`;
+  document.querySelector("#x").textContent = x;
+  document.querySelector("#y").textContent = y;
+}
+
+document.addEventListener("keydown", keydown);
+document.addEventListener("keyup", keyup);
+document.addEventListener("click", click);
+eventType.addEventListener("change", toggleDisplay);
